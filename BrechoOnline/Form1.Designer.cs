78d9de98cb13818b41b779480e2376d5f40cadfa
cj3016579 <cj3016579@LAB01-PC05>
2023-08-09 12:44:29 -0300

    Adicionar arquivos de projeto.

---
+++ BrechoOnline/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BrechoOnline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblStyle = new System.Windows.Forms.Label();
+            this.txbStyle = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnLogin.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(443, 232);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(127, 49);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login\r\n";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(12, 28);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(229, 20);
+            this.txbName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(9, 12);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(107, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Nome completo";
+            this.lblName.Click += new System.EventHandler(this.Nome_Click);
+            // 
+            // lblStyle
+            // 
+            this.lblStyle.AutoSize = true;
+            this.lblStyle.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStyle.Location = new System.Drawing.Point(9, 72);
+            this.lblStyle.Name = "lblStyle";
+            this.lblStyle.Size = new System.Drawing.Size(42, 15);
+            this.lblStyle.TabIndex = 3;
+            this.lblStyle.Text = "Estilo";
+            // 
+            // txbStyle
+            // 
+            this.txbStyle.Location = new System.Drawing.Point(12, 88);
+            this.txbStyle.Name = "txbStyle";
+            this.txbStyle.Size = new System.Drawing.Size(229, 20);
+            this.txbStyle.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 130);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Cliente";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(12, 153);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Vendedor";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(582, 293);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txbStyle);
+            this.Controls.Add(this.lblStyle);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStyle;
+        private System.Windows.Forms.TextBox txbStyle;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ BrechoOnline/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrechoOnline
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string name = txbName.Text;
+            string style = txbStyle.Text;
+
+            string message = "Nome: " + name + "\nEstilo: " + style;
+
+            MessageBox.Show(
+                message,
+                "ATENÇÃO!", 
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+        }
+
+        private void Nome_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
