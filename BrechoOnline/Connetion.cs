9a98a6424af446c294836aa06ca8388724638aa3
M-Godoy <godoy.s@aluno.ifsp.edu.br>
2023-08-30 12:51:06 -0300

    dia 30-08

---
+++ BrechoOnline/Connetion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace brechoonline
+{ 
+ 
+    internal class Connection
+    {
+        //Propriedades ou atributos
+        private readonly SqlConnection con;
+        private readonly string DataBase = "Brechó_Online";
+
+        //Construtor
+        public Connection()                            //LAPTOP-9TN8R1CF\SQLEXPRESS
+        {
+            //Data Source=LAPTOP-9TN8R1CF\SQLEXPRESS;Initial Catalog=InvestimentosMais;Integrated Security=True
+            string stringConnection = @"Data Source="
+                    + Environment.MachineName +
+                    @"\SQLEXPRESS;Initial Catalog=" +
+                    DataBase + ";Integrated Security=true";
+
+            con = new SqlConnection(stringConnection);
+            con.Open();   //Abrir a conexão com o banco de dados
+        }
+        //Tenta fechar a conexão com o banco
+        public void CloseConnection()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+        //Retorna a conexão que foi aberta
+        public SqlConnection ReturnConnection()
+        {
+            return con;
+        }
+
+    }
+
+}
+
+
--- BrechoOnline/Form1.cs
+++ BrechoOnline/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Xml.Linq;
 using System.Security.Authentication.ExtendedProtection;
+using brechoonline;
+using System.Data.SqlClient;
 
 namespace BrechoOnline
 {
@@ -22,36 +24,58 @@ namespace BrechoOnline
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if (ckbServiceterms.Checked)
-            {
-                if (ckbSeller.Checked || ckbClient.Checked)
-                {
-                    string name = txbName.Text;
-                    string email = txbEmail.Text;
-                    string contact = txbContat.Text;
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
 
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = "INSERT INTO Cadastro VALUES (@NOME_COMPLETO, @EMAIL, @SENHA, @CONTATO)";
 
-                    string message = "Nome: " + name + "\nEmail: " + email + "\nContato: " + contact;
+            sqlCommand.Parameters.AddWithValue("@NOME", txbName.Text);
+            sqlCommand.Parameters.AddWithValue("@EMAIL", txbEmail.Text);
+            sqlCommand.Parameters.AddWithValue("@SENHA", txbPassword.Text);
+            sqlCommand.Parameters.AddWithValue("@CONTATO", txbContat.Text);
 
-                    MessageBox.Show(
-                        message,
-                        "ATENÇÃO!",
-                        MessageBoxButtons.OK,
-                    MessageBoxIcon.Information
-                        );
-                }
-            }
-            else
-            {
-                string message = "-Você precisa aceitar nossos termos de privacidade e preencher o tipo de usuário antes de concluir o cadastro";
+            sqlCommand.ExecuteNonQuery();
 
-                MessageBox.Show(
-                    message, 
-                    "ATENÇÃO!",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information
-                    );
-            }
+            //if (ckbServiceterms.Checked)
+            //{
+            //    if (ckbSeller.Checked || ckbClient.Checked)
+            //    {
+            //        string name = txbName.Text;
+            //        string email = txbEmail.Text;
+            //        string contact = txbContat.Text;
+            //        string message = "Nome: " + name + "\nEmail: " + email + "\nContato: " + contact;
+
+            //        MessageBox.Show(
+            //            message,
+            //            "ATENÇÃO!",
+            //            MessageBoxButtons.OK,
+            //        MessageBoxIcon.Information
+            //            );
+            //    }
+            //    else
+            //    {
+            //        string message = "-Você precisa aceitar nossos termos de privacidade e preencher o tipo de usuário antes de concluir o cadastro";
+
+            //        MessageBox.Show(
+            //            message,
+            //            "ATENÇÃO!",
+            //            MessageBoxButtons.OK,
+            //            MessageBoxIcon.Information
+            //            );
+            //    }
+            //}
+            //else
+            //{
+            //    string message = "-Você precisa aceitar nossos termos de privacidade e preencher o tipo de usuário antes de concluir o cadastro";
+
+            //    MessageBox.Show(
+            //        message,
+            //        "ATENÇÃO!",
+            //        MessageBoxButtons.OK,
+            //        MessageBoxIcon.Information
+            //        );
+            //}
         }
 
         private void Nome_Click(object sender, EventArgs e)
