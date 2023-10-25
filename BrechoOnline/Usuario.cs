5b49e79e07dd8ac54f0c4338d171bd13fe881428
M-Godoy <godoy.s@aluno.ifsp.edu.br>
2023-10-25 12:25:55 -0300

    consertado e atualizado

--- BrechoOnline/Usuario.cs
+++ BrechoOnline/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
@@ -9,44 +10,69 @@ namespace BrechoOnline
 {
     public class Usuario
     {
-        private string _id;
+        private int _id;
         private string _nome_completo;
         private string _email;
         private string _senha;
         private string _contato;
 
-        public Usuario(string nome_completo, string email, string senha, string contato)
+        public Usuario(int id, string nome_completo, string email, string senha, string contato)
         {
+            _id = id;
             _nome_completo = nome_completo;
-            _email = email;  
+            _email = email;
             _senha = senha;
             _contato = contato;
         }
-        public string NOME_COMPLETO 
-        {
-            set { _nome_completo = value; }
-            get { return _nome_completo; }
-        } 
-        public string EMAIL 
-        {
-            set { _email = value; }
-            get { return _email; }
-        } 
-        public string SENHA 
-        {
-            set { _senha = value; }
-            get { return _senha; }
-        } 
-        public string CONTATO
-        {
-            set { _contato = value; }
-            get { return _contato; }
-        }
-        public string ID
+
+        public int ID
         {
             set { _id = value; }
             get { return _id; }
+        
         }
+        public string NOME_COMPLETO
+        {
+            set
+            {
+                if
+            (string.IsNullOrEmpty(value))
+                    throw new Exception("Campo nome está vazio");
+                _nome_completo = value;
+            }
+            get { return _nome_completo; }
+        }
+        public string EMAIL
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Campo email está vazio");
+                _email = value;
+            }
+            get { return _email; }
+        }
+        public string SENHA
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Campo senha está vazio");
+                _senha = value;
+            }
+            get { return _senha; }
+        }
+        public string CONTATO
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Campo contato está vazio");
+                _contato = value;
+            }
+            get { return _contato; }
+        }
+
 
     }
 }
--- BrechoOnline/UsuarioDAO.cs
+++ BrechoOnline/UsuarioDAO.cs
@@ -1,5 +1,7 @@
 ﻿using brechoonline;
+using BrechoOnline;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -7,14 +9,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace BrechoOnline
 {
+    //DAO = Data Access Objct. 
+    //Essa é a classe que vai [ligar] com o BD. 
     internal class UsuarioDAO
     {
+        //(1) 
+        //void é quando não tem que retornar nada. 
+
+        //select
+        public List<Usuario> SelectUser()
+        {
+            Connection conn = new Connection();
+            SqlCommand sqlCom = new SqlCommand();
+
+            sqlCom.Connection = conn.ReturnConnection();
+            sqlCom.CommandText = "SELECT * FROM Table_1";
+
+            List<Usuario> users = new List<Usuario>();
+            try
+            {
+                SqlDataReader dr = sqlCom.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute. 
+                while (dr.Read())
+                {
+                    Usuario objeto = new Usuario( 
+                        (int)dr["ID"],
+                        (string)dr["NOME_COMPLETO"],
+                        (string)dr["EMAIL"],
+                        (string)dr["SENHA"],
+                        (String)dr["CONTATO"]
+                        );
+
+                    users.Add(objeto);
+                }
+                dr.Close();
+                
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro na Leitura de Dados.\n" + err.Message);
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+            return users;//Retornar a lista.
+        }
+        //insert
+        public void InsertUser(Usuario user)
+        {
+            //esse Connection verde água é o nome da sua classe. 
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO Table_1 VALUES (@id, @nome_completo, @email, @senha, @contato)";
+
+            sqlCommand.Parameters.AddWithValue("@id", user.ID); 
+            sqlCommand.Parameters.AddWithValue("@nome_completo", user.NOME_COMPLETO);
+            sqlCommand.Parameters.AddWithValue("@email", user.EMAIL);
+            sqlCommand.Parameters.AddWithValue("@senha", user.SENHA); 
+            sqlCommand.Parameters.AddWithValue("@contato", user.CONTATO);
+
+            sqlCommand.ExecuteNonQuery();
+        }
+        //update
+        public void UpdateUser(Usuario user)
+        {
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"UPDATE Table_1 SET  
+             NOME_COMPLETO      = @nome_completo,  
+             EMAIL        = @email,  
+             SENHA   = @senha
+             CONTATO = @contato
+             WHERE ID   = @id";
+
+            //idêntico ao do botão insert 
+
+            sqlCommand.Parameters.AddWithValue("@id", user.ID);
+            sqlCommand.Parameters.AddWithValue("@nome_completo", user.NOME_COMPLETO);
+            sqlCommand.Parameters.AddWithValue("@email", user.EMAIL);
+            sqlCommand.Parameters.AddWithValue("@senha", user.SENHA);
+            sqlCommand.Parameters.AddWithValue("@contato", user.CONTATO);
+
+
+            sqlCommand.ExecuteNonQuery();
+        }
+        //delete 
         public void DeleteUser(int id)
         {
-            //Código para excluir
+            //Código para excluir 
             Connection connection = new Connection();
             SqlCommand sqlCommand = new SqlCommand();
 
